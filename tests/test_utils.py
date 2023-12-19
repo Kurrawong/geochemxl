4d84e99f51666935fdcfba3d2998d5da06bc38ad
Nicholas Car <nick@kurrawong.net>
2023-12-19 16:03:53 +1000

    initial commit - all tests passing

---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+from geochemxl.utils import *
+
+CONCEPTS_COMBINED_GRAPH = Graph().parse(Path(__file__).parent / "data" / "concepts-combined-3.0.ttl")
+
+
+def test__get_codelist_id_for_code():
+    assert "LENGTH" in get_codelist_ids_for_code("m", CONCEPTS_COMBINED_GRAPH)
+
+    assert "LOC_SURVEY_TYPE" in get_codelist_ids_for_code("DGPS", CONCEPTS_COMBINED_GRAPH)
